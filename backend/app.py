0a53f4ab2ba977b4a6dff466c6af2df9d8751fac
suria <132283516+cuyaya@users.noreply.github.com>
2025-10-13 15:52:26 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,13 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)  # allow frontend to access backend
+
+# Example endpoint
+@app.route('/api/test', methods=['GET'])
+def test():
+    return jsonify({"message": "IntegriFy backend is running!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
