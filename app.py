cafebc9b3a4859f34eabb70fd3ab16996df0b7ad
suria <132283516+cuyaya@users.noreply.github.com>
2025-12-28 18:49:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import base64
 import shap
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://integrify-dc501.web.app"])
 
 # -------------------------
 # Configuration
@@ -232,5 +232,5 @@ def handle_audio():
 # Run Flask App
 # -------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use DO's PORT if available
+    port = int(os.environ.get("PORT", 8000))  # Use DO's PORT if available
     app.run(host="127.0.0.1", port=port)
