22e243a5d9a32a8e3c16a1d365f2f77786d31aa9
suria <132283516+cuyaya@users.noreply.github.com>
2026-01-08 23:32:39 +0800

    Delete frontend directory

--- frontend/app.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('testButton').addEventListener('click', async () => {
-  const response = await fetch('http://127.0.0.1:5000/api/test');
-  const data = await response.json();
-  document.getElementById('response').textContent = data.message;
-});
